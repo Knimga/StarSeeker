8a1ee571858d09c52268fd8d9375e8a59ca4bdb1
Evan <egrulke113@gmail.com>
2020-02-07 20:49:26 -0700

    fixed config in users route

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-const config = require('../config/db');
+
+const config = process.env.HEROKU_MONGO ? JSON.parse(process.env.HEROKU_MONGO) : require('./config/db');
 
 const User = require('../models/user.model');
 
